0f54816fcda80bcf9f9095c91aadeb9e7a92d6e2
Robocat <neatfx@gmail.com>
2020-05-18 23:03:12 +0800

    refact: menu @ main process

--- main/app-menu.ts
+++ main/app-menu.ts
@@ -1,18 +1,21 @@
 import darwinMenu from './menus/darwin'
-// import ('./menus/linux')
-// import ('./menus/win32')
-import { ComponentsRouter } from './index'
+import linuxMenu from './menus/linux'
+import win32Menu from './menus/win32'
 
-export default function(cr: ComponentsRouter) {
-  if (process.platform === 'darwin'){
-    darwinMenu(cr)
+export default (() => {
+  if (process.platform === 'darwin') {
+    return darwinMenu
   }
 
-  if (process.platform === 'linux'){
-
+  if (process.platform === 'linux') {
+    return linuxMenu
   }
 
-  if (process.platform === 'win32'){
-
+  if (process.platform === 'win32') {
+    return win32Menu
   }
-}
+
+  return () => {
+    console.log('unsupported platform!')
+  }
+})()
