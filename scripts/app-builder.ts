8e45ef3f214ec6ad3aa526d4790875c1cc6958e0
Robocat <neatfx@gmail.com>
2020-06-05 02:38:14 +0800

    test: gh-action

--- scripts/app-builder.ts
+++ scripts/app-builder.ts
@@ -1,39 +1,39 @@
 import { build as viteBuild } from 'vite'
-import { build as esbuild } from 'esbuild'
+// import { build as esbuild } from 'esbuild'
 import { build as electronBuild, Platform } from 'electron-builder'
 import { execSync, spawn } from 'child_process'
 
-import esbuildConfig from '../configs/esbuild.config'
+// import esbuildConfig from '../configs/esbuild.config'
 import viteConfig from '../configs/vite.config'
 import electronBuilderConfig from '../configs/electron-builder'
 
-// const run = (cmd: string, cwd: string) => execSync(cmd, { encoding: "utf8", stdio: "inherit", cwd })
+const run = (cmd: string, cwd: string) => execSync(cmd, { encoding: "utf8", stdio: "inherit", cwd })
 
 async function packMain () {
-  return new Promise((resolve, reject) => {
-    const args = [
-      '--platform=node',
-      '--bundle',
-      '--minify',
-      '--external:electron',
-      '--external:path',
-      '--external:fs',
-      'main/main.ts',
-      'main/preload.ts',
-      '--outdir=build'
-    ]
-
-    spawn('esbuild', args, {
-      stdio: 'inherit'
-    }).on('close', () => {
-      resolve()
-    })
-  })
-
   // return new Promise((resolve, reject) => {
-  //   run('esbuild --platform=node --bundle --minify --external:electron --external:path --external:fs main/main.ts main/preload.ts --outdir=build', './')
-  //   resolve()
+  //   const args = [
+  //     '--platform=node',
+  //     '--bundle',
+  //     '--minify',
+  //     '--external:electron',
+  //     '--external:path',
+  //     '--external:fs',
+  //     'main/main.ts',
+  //     'main/preload.ts',
+  //     '--outdir=build'
+  //   ]
+
+  //   spawn('esbuild', args, {
+  //     stdio: 'inherit'
+  //   }).on('close', () => {
+  //     resolve()
+  //   })
   // })
+
+  return new Promise((resolve, reject) => {
+    run('esbuild --platform=node --bundle --minify --external:electron --external:path --external:fs main/main.ts main/preload.ts --outdir=build', './')
+    resolve()
+  })
 
   // try {
   //   const result = await esbuild(esbuildConfig.dist)
