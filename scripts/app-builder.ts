19acd97d00428ebe574ad8553c9e9412b82b31e0
Robocat <neatfx@gmail.com>
2020-06-04 23:34:16 +0800

    test: gh-action build

--- scripts/app-builder.ts
+++ scripts/app-builder.ts
@@ -1,41 +1,41 @@
 import { build as viteBuild } from 'vite'
-// import { build as esbuild } from 'esbuild'
+import { build as esbuild } from 'esbuild'
 import { build as electronBuild, Platform } from 'electron-builder'
-import { spawn } from 'child_process'
+// import { spawn } from 'child_process'
 
-// import esbuildConfig from '../configs/esbuild.config'
+import esbuildConfig from '../configs/esbuild.config'
 import viteConfig from '../configs/vite.config'
 import electronBuilderConfig from '../configs/electron-builder'
 
 function packMain () {
-  return new Promise((resolve, reject) => {
-    const args = [
-      '--platform=node',
-      '--bundle',
-      '--minify',
-      '--external:electron',
-      '--external:path',
-      '--external:fs',
-      'main/main.ts',
-      'main/preload.ts',
-      '--outdir=build'
-    ]
+  // return new Promise((resolve, reject) => {
+  //   const args = [
+  //     '--platform=node',
+  //     '--bundle',
+  //     '--minify',
+  //     '--external:electron',
+  //     '--external:path',
+  //     '--external:fs',
+  //     'main/main.ts',
+  //     'main/preload.ts',
+  //     '--outdir=build'
+  //   ]
 
-    spawn('esbuild', args, {
-      stdio: 'inherit'
-    }).on('close', () => {
-      resolve()
-    })
+  //   spawn('esbuild', args, {
+  //     stdio: 'inherit'
+  //   }).on('close', () => {
+  //     resolve()
+  //   })
+  // })
+
+  return esbuild(esbuildConfig.dist).then(result => {
+    console.log(result.stderr)
   })
-
-  // return esbuild(esbuildConfig.dist).then(result => {
-  //   console.log(result.stderr)
-  // })
-  // .catch(err => {
-  //   console.log(`\nfailed to build main process`)
-  //   console.error(`\n${err}\n`)
-  //   process.exit(1)
-  // })
+  .catch(err => {
+    console.log(`\nfailed to build main process`)
+    console.error(`\n${err}\n`)
+    process.exit(1)
+  })
 }
 
 function packRenderer() {
