fd3a1b19192057c9e0d5a1623404f2dbda969cbc
Robocat <neatfx@gmail.com>
2020-06-04 20:45:06 +0800

    test: gh-action

--- scripts/app-builder.js
+++
@@ -1,3 +0,0 @@
-const { execSync } = require("child_process")
-const run = (cmd, cwd) => execSync(cmd, { encoding: "utf8", stdio: "inherit", cwd })
-run('esbuild --platform=node --bundle --external:vite --external:esbuild --external:electron-builder scripts/app-builder.ts --outfile=build/app-builder.js', '.')
