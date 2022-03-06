4d96f40368aa32fb09eddc3dac1fdf709d1a6809
Robocat <neatfx@gmail.com>
2022-03-06 22:48:20 +0800

    update

--- renderer/router.ts
+++ renderer/router.ts
@@ -1,20 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-import ViewA from './pages/ViewA.vue'
-import ViewB from './pages/ViewB.vue'
-import ViewC from './pages/ViewC.vue'
-import ViewD from './pages/ViewD.vue'
-import Preferences from './pages/Preferences.vue'
-
-const routerHistory = createWebHashHistory()
 const router = createRouter({
-  history: routerHistory,
+  history: createWebHashHistory(),
   routes: [
-    { path: '/', component: ViewA as never },
-    { path: '/b', component: ViewB as never },
-    { path: '/c', component: ViewC as never },
-    { path: '/d', component: ViewD as never },
-    { path: '/preferences', component: Preferences as never }
+    { path: '/', component: () => import('./pages/ViewA.vue')},
+    { path: '/b', component: () => import('./pages/ViewB.vue')},
+    { path: '/c', component: () => import('./pages/ViewC.vue')},
+    { path: '/d', component: () => import('./pages/ViewD.vue')},
+    { path: '/preferences', component: () => import('./pages/Preferences.vue')}
   ]
 })
 
