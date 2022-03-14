21bd84c643767f2ac545feb933c0b3301a7425a4
Robocat <neatfx@gmail.com>
2022-03-14 19:26:58 +0800

    update

--- renderer/views/Preferences.vue
+++ renderer/views/Preferences.vue
@@ -1,26 +1,11 @@
+<script setup lang="ts">
+</script>
+
 <template>
   <div>
-    dsfdsfdss
+    ViewPreferences
   </div>
 </template>
-
-<script lang="ts">
-export default {
-//   props: {
-//   },
-//   setup(props) {
-//     const info = ref({})
-//     onMounted(() => {
-//       const returnValue = ipcApi.sendSync('get-sys-info', 'ping')
-//       console.log('[get-sys0-info]', returnValue)
-//     })
-
-//     return {
-//       info
-//     }
-//   }
-}
-</script>
 
 <style scoped>
 #nav {
--- renderer/views/ViewC.vue
+++ renderer/views/ViewC.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineComponent } from "vue"
+import {} from "vue"
 </script>
 
 <template>
