0f2ede7dea3801663fccbab00e4579ecddf61843
Robocat <neatfx@gmail.com>
2022-03-17 19:29:13 +0800

    u

--- renderer/components/PrefDBExportImport.vue
+++ renderer/components/PrefDBExportImport.vue
@@ -2,7 +2,7 @@
 import { onMounted, ref } from 'vue'
 import { TestingDB } from '../db'
 import BaseButton from './BaseButton.vue';
-import {importDB, exportDB, importInto, peakImportFile} from "dexie-export-import";
+import { importDB, exportDB, importInto, peakImportFile } from "dexie-export-import";
 import type { ExportProgress } from 'dexie-export-import/dist/export';
 
 type Table = {
@@ -12,19 +12,40 @@ type Table = {
   primKey: string;
 }
 let tables = ref<Table[]>([])
+let fileBlob: Blob;
+const enableImportBtn = ref(false)
 
 async function exportDatabase() {
   try {
-    const blob = await exportDB(TestingDB, { prettyJson: true , progressCallback});
+    const blob = await exportDB(TestingDB, { prettyJson: true, progressCallback });
     console.log(blob)
   } catch (error) {
     console.error('' + error);
   }
-};
+}
+
+async function importDatabase() {
+  console.log('importing...')
+  // await TestingDB.delete();
+  // await importDB(fileBlob, { progressCallback });
+  // console.log("Import complete");
+
+  // await showContent();
+}
 
 function progressCallback(progress: ExportProgress): boolean {
   console.log(`Progress: ${progress.completedRows} of ${progress.totalRows} rows completed`);
   return progress.done
+}
+
+async function peakImportInfo(blob: Blob) {
+  const importMeta = await peakImportFile(blob);
+
+  console.log("Database name:", importMeta.data.databaseName);
+  console.log("Database version:", importMeta.data.databaseVersion);
+  console.log("Tables:", importMeta.data.tables.map(t =>
+    `${t.name} (${t.rowCount} rows)`
+  ).join('\n\t'));
 }
 
 function ondragover(event: any) {
@@ -33,25 +54,21 @@ function ondragover(event: any) {
   event.dataTransfer.dropEffect = 'copy';
 };
 
-// Handle file drop
 async function ondrop(ev: any) {
   ev.stopPropagation();
   ev.preventDefault();
 
-  // Pick the File from the drop event (a File is also a Blob):
   const file = ev.dataTransfer.files[0];
-  // try {
-  //   if (!file) throw new Error(`Only files can be dropped here`);
-  //   console.log("Importing " + file.name);
-  //   await db.delete();
-  //   db = await Dexie.import(file, {
-  //     progressCallback
-  //   });
-  //   console.log("Import complete");
-  //   await showContent();
-  // } catch (error) {
-  //   console.error('' + error);
-  // }
+  try {
+    if (!file) throw new Error(`Only files can be dropped here`);
+    console.log("Importing " + file.name);
+    peakImportInfo(file)
+
+    fileBlob = file
+    enableImportBtn.value = true
+  } catch (error) {
+    console.error('' + error);
+  }
 }
 
 async function showContent() {
@@ -69,22 +86,24 @@ onMounted(async () => {
 </script>
 
 <template>
-  <h5>Database Name: {{ TestingDB.name }}</h5>
+  <h5>Database Name - {{ TestingDB.name }}</h5>
   <ul>
     <li v-for="table in tables" :key="table.id">
       <span>Table: {{ table.id }} - Primary Key: {{ table.primKey }} - Row count: {{ table.count }}</span>
     </li>
   </ul>
   <BaseButton text="Export Database" @click="exportDatabase"></BaseButton>
-  <div id="dropzone" @dragover="ondragover" @drop="ondrop">Drop exported Dexie JSON file here.</div>
+  <BaseButton v-if="enableImportBtn" text="Import Database" @click="importDatabase"></BaseButton>
+  <div id="dropzone" @dragover="ondragover" @drop="ondrop">Drop Dexie JSON file here.</div>
 </template>
 
 <style scoped>
 #dropzone {
-  border: 3px dotted grey;
+  border: 1px dashed grey;
   border-radius: 10px;
-  padding: 25px;
-  margin-top: 20px;
+  width: 250px;
+  padding: 30px 20px;
+  margin: 20px 0;
   color: grey;
   text-align: center;
 }
