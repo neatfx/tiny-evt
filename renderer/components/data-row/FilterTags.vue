9eb64a843c0e7abcf4b315ba4e8cb1cac8020508
Robocat <neatfx@gmail.com>
2022-03-24 18:10:28 +0800

    u

--- renderer/components/data-row/FilterTags.vue
+++ renderer/components/data-row/FilterTags.vue
@@ -7,57 +7,63 @@ defineProps(['items'])
 </script>
 
 <template>
-  <ul id="filter-tags-wrapper" v-if="Object.keys(items).length">
-    <li>
-      <BaseButton @click="resetFilter">Remove All Filters</BaseButton>
-    </li>
-    <li v-for="(v, k) in items" :key="k" class="filter-item">
-      <span class="filter-type">{{ k }}</span>
-      <span class="filter-type">is</span>
-      <span class="filter-value">{{ v }}</span>
-      <span class="btn-delete" @click="removeFilter(k.toString())">
-        <!-- <span class="icon-cross"></span> -->
-        X
-      </span>
-    </li>
-  </ul>
+  <div class="wrapper" v-if="Object.keys(items).length">
+    <BaseButton @click="resetFilter">Clear All Filters</BaseButton>
+    <ul class="filter-tags-wrapper">
+      <li v-for="(v, k) in items" :key="k">
+        <div class="filter-type">{{ k }}</div>
+        <span class="filter-is">is</span>
+        <span class="filter-value">{{ v }}</span>
+        <span class="btn-delete" @click="removeFilter(k.toString())">X</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
 /* 过滤器标签 */
-#filter-tags-wrapper {
+.wrapper {
+  border: 1px solid grey;
+  padding: 10px;
+  box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.1);
+}
+.filter-tags-wrapper {
   display: grid;
-  /* grid-template-columns: auto; */
   grid-auto-flow: column;
   gap: 10px;
   list-style: none;
   justify-content: left;
-  padding: 10px;
-  margin: 0;
-  border: 1px solid grey;
+  padding: 0px;
+  margin: 10px 0 0 0;
+  /* border: 1px solid grey; */
 }
 li {
-  background-color: cadetblue;
-}
-.filter-item {
-  margin-right: 10px;
+  border: 1px solid grey;
+  padding: 2px;
+  box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.1);
 }
 .filter-type {
   display: inline-block;
+  padding: 2px 15px;
+  margin-right: 2px;
+  background-color: slategray;
+}
+.filter-is {
+  display: inline-block;
   padding: 2px 10px;
-  margin-right: 1px;
-  background-color: darkkhaki;
+  margin-right: 2px;
+  background-color: grey;
 }
 .filter-value {
   display: inline-block;
-  padding: 2px 10px 2px;
-  background-color: cadetblue;
+  padding: 2px 15px 2px;
+  background-color: slategray;
 }
 .btn-delete {
   display: inline-block;
   padding: 2px 10px 2px 10px;
-  margin-left: 1px;
-  background-color: indianred;
+  margin-left: 2px;
+  background-color: rosybrown;
 }
 .icon-cross {
   /* font-size:large; */
