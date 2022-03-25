a44d3dab1e56d1619a66642d550a0af08fe16015
Robocat <neatfx@gmail.com>
2022-03-26 06:09:13 +0800

    u

--- renderer/components/data-row/contacts/ContactRowsViewOptions.vue
+++ renderer/components/data-row/contacts/ContactRowsViewOptions.vue
@@ -26,24 +26,48 @@ function toggle(field: string) {
       <BaseButton>View</BaseButton>
     </template>
     <template #body>
-      <ul class="wrapper">
-        <li v-for="(v, k) in store.view" :key="k">
-          <div :class="['status-color-base', v ? on : off]"></div>
-          <BaseButton @click="toggle(k)">{{ k }}</BaseButton>
-        </li>
-      </ul>
+      <div class="wrapper">
+        <div class="controller-block">Controller</div>
+        <ul>
+          <li v-for="(v, k) in store.view" :key="k">
+            <div :class="['status-color-base', v ? on : off]"></div>
+            <BaseButton @click="toggle(k)">{{ k }}</BaseButton>
+          </li>
+        </ul>
+        <hr>
+        <div class="fields-block">Fields</div>
+        <ul>
+          <li v-for="(v, k) in Object.keys(store.items[0])" :key="k">
+            <div :class="['status-color-base', true ? on : off]"></div>
+            <BaseButton @click="toggle(v)">{{ v }}</BaseButton>
+          </li>
+        </ul>
+      </div>
     </template>
   </FolderPanel>
 </template>
 
 <style scoped>
 .wrapper {
+  padding: 20px;
+}
+hr{
+  margin: 15px 0 0;
+  border:1px solid darkgrey;
+}
+.controller-block {
+  margin: 0 0 10px 0;
+}
+.fields-block {
+  margin: 10px 0 10px 0;
+}
+ul {
   display: grid;
   grid-auto-flow: column;
   gap: 10px;
   list-style: none;
   justify-content: left;
-  padding: 10px;
+  padding: 0px;
   margin: 0px 0 0 0;
 }
 li {
