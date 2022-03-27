73ae5f43a82c0b267a0748d9475d4ad414b81548
Robocat <neatfx@gmail.com>
2022-03-27 23:05:45 +0800

    中文分词 population

--- renderer/db/testing/population.ts
+++ renderer/db/testing/population.ts
@@ -5,55 +5,57 @@
 import { TestingDB } from './index';
 import { Contact } from './contact';
 import { Book } from './book';
-// import nodejieba from '@node-rs/jieba';
+import { Segment, useDefault } from 'segmentit'
+
+const segmentit = useDefault(new Segment());
 
 async function populateBooks() {
   const book1 = new Book('流浪地球', '刘慈欣')
   book1.categories = ['科幻']
   book1.publishing = '长江文艺出版社'
   book1.published = '2000'
-  // book1.nameWords = nodejieba.cut(book1.name)
-  book1.nameWords = ['流浪', '地球', '刘慈欣', '科幻']
+  book1.nameWords = segmentit.doSegment(book1.name, { simple: true })
+  book1.nameWords = book1.nameWords?.concat([book1.author, ...book1.categories])
 
   const book2 = new Book('胶东雄狮', '彦庆杰')
   book2.categories = ['历史']
   book2.publishing = '黄河出版社'
-  // book2.nameWords = nodejieba.cut(book2.name)
-  book2.nameWords = ['胶东', '雄狮', '彦庆杰','历史']
+  book2.nameWords = segmentit.doSegment(book2.name, { simple: true })
+  book2.nameWords = book2.nameWords?.concat([book2.author, ...book2.categories])
 
   const book3 = new Book('三体', '刘慈欣')
   book3.categories = ['科幻']
   book3.publishing = '重庆出版社'
   book3.published = '2006'
-  // book3.nameWords = nodejieba.cut(book3.name)
-  book3.nameWords = ['三体', '刘慈欣', '科幻']
+  book3.nameWords = segmentit.doSegment(book3.name, { simple: true })
+  book3.nameWords = book3.nameWords?.concat([book3.author, ...book3.categories])
 
   const book4 = new Book('三体2:黑暗森林', '刘慈欣')
   book4.categories = ['科幻']
   book4.publishing = '重庆出版社'
   book4.published = '2008'
-  // book4.nameWords = nodejieba.cut(book4.name)
-  book4.nameWords = ['三体', '黑暗', '森林', '刘慈欣', '科幻']
+  book4.nameWords = segmentit.doSegment(book4.name, { simple: true })
+  book4.nameWords = book4.nameWords?.concat([book4.author, ...book4.categories])
 
   const book5 = new Book('三体3:死神永生', '刘慈欣')
   book5.categories = ['科幻']
   book5.publishing = '重庆出版社'
   book5.published = '2010'
-  // book5.nameWords = nodejieba.cut(book5.name)
-  book5.nameWords = ['三体', '死神', '永生', '刘慈欣', '科幻']
+  book5.nameWords = segmentit.doSegment(book5.name, { simple: true })
+  book5.nameWords = book5.nameWords?.concat([book5.author, ...book5.categories])
 
   const book6 = new Book('中国近代史', '徐中约')
   book6.categories = ['历史', '中国', '近代史']
   book6.publishing = '后浪出版社'
-  // book6.nameWords = nodejieba.cut(book6.name)
-  book6.nameWords = ['中国', '近代史', '徐中约', '历史']
+  book6.nameWords = segmentit.doSegment(book6.name, { simple: true })
+  book6.nameWords = book6.nameWords?.concat([book6.author, ...book6.categories])
 
   const book7 = new Book('Under the Dome', 'Stephen King')
   book7.categories = ['科幻', '惊悚', '小说']
   book7.publishing = '斯克里布纳之子公司'
   book7.published = '2009年11月10日'
-  // book7.nameWords = nodejieba.cut(book7.name)
-  book7.nameWords = ['under', 'dome', 'stephen', 'king', '小说']
+  book7.nameWords = segmentit.doSegment(book7.name, { simple: true })
+  book7.nameWords = book7.nameWords?.concat([book7.author, ...book7.categories])
 
   await TestingDB.books.bulkAdd([
     book1, book2, book3, book4, book5, book6, book7
