f1a465d19c67dabfe1cffef2337f3c7359b50383
Robocat <neatfx@gmail.com>
2022-03-28 21:28:52 +0800

    分词修复

--- renderer/db/testing/search.ts
+++ renderer/db/testing/search.ts
@@ -4,28 +4,26 @@
 
 import { TestingDB } from './index';
 import type { IBook } from "./type-defs";
-import { Segment, URLTokenizer, WildcardTokenizer, PunctuationTokenizer, ForeignTokenizer, DictTokenizer, DictOptimizer } from 'segmentit'
+import { Segment, URLTokenizer, WildcardTokenizer, PunctuationTokenizer, ForeignTokenizer, DictTokenizer, ChsNameTokenizer, DictOptimizer, ChsNameOptimizer, DatetimeOptimizer, pangu, panguExtend1, panguExtend2, wildcard } from 'segmentit'
 
 export const segmentit = new Segment();
 // 强制分割类单词识别
-// segmentit.use(URLTokenizer); // URL识别
+segmentit.use(URLTokenizer); // URL识别
 segmentit.use(WildcardTokenizer); // 通配符，必须在标点符号识别之前
 segmentit.use(PunctuationTokenizer); // 标点符号识别
 segmentit.use(ForeignTokenizer); // 外文字符、数字识别，必须在标点符号识别之后
 // 中文单词识别
 segmentit.use(DictTokenizer); // 词典识别
-// segmentit.use(ChsNameTokenizer); // 人名识别，建议在词典识别之后
+segmentit.use(ChsNameTokenizer); // 人名识别，建议在词典识别之后
 // 优化模块
 segmentit.use(DictOptimizer) // 词典识别优化
-// segmentit.use(ChsNameOptimizer); // 人名识别优化
-// segmentit.use(DictOptimizer); // 词典识别优化
-// segmentit.use(DatetimeOptimizer); // 日期时间识别优化
+segmentit.use(ChsNameOptimizer); // 人名识别优化
+segmentit.use(DatetimeOptimizer); // 日期时间识别优化
 // 字典文件
-// segmentit.loadDict('dict.txt'); // 盘古词典
-// segmentit.loadDict('dict2.txt'); // 扩展词典（用于调整原盘古词典）
-// segmentit.loadDict('names.txt'); // 常见名词、人名
-// segmentit.loadDict('wildcard.txt', 'WILDCARD', true); // 通配符
-// segmentit.loadSynonymDict('synonym.txt'); // 同义词
+segmentit.loadDict(pangu); // 盘古词典
+// segmentit.loadDict(panguExtend1, panguExtend2); // 扩展词典（用于调整原盘古词典）
+segmentit.loadDict(wildcard, 'WILDCARD', true); // 通配符
+// segmentit.loadSynonymDict(synonym); // 同义词
 // segmentit.loadStopwordDict('stopword.txt'); // 停止符
 
 function hookCreating() {
