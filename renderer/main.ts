f87eb0ab24c82e9f404418e4029d73122c54c941
Robocat <neatfx@gmail.com>
2022-04-12 22:59:52 +0800

    u

--- renderer/main.ts
+++ renderer/main.ts
@@ -6,6 +6,13 @@ import router from './router'
 import './assets/base.css'
 import 'uno.css'
 
+// normalize.css
+import '@unocss/reset/normalize.css'
+// reset.css by Eric Meyer https://meyerweb.com/eric/tools/css/reset/index.html
+// import '@unocss/reset/eric-meyer.css'
+// preflights from tailwind
+// import '@unocss/reset/tailwind.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
