ed81bcf4c94dc6adbc41236c5c5ba177874efc3a
Robocat <neatfx@gmail.com>
2022-04-17 21:42:38 +0800

    修复 filter 内部计数逻辑

--- renderer/components/controller-bar/filter.ts
+++ renderer/components/controller-bar/filter.ts
@@ -1,29 +1,34 @@
-import { reactive, ref } from "vue"
+import { computed } from "@vue/reactivity"
+import { reactive } from "vue"
 
-const filtersCount = ref(0)
 const workingFilters = reactive<Map<string, Set<string>>>(new Map())
+const filtersCount = computed(() => {
+  let count = 0
+
+  workingFilters.forEach((v, k) => {
+    count += v.size
+  })
+
+  return count
+})
 
 function filter(type: string, value: string, multiSelection: boolean = true) {
   if (!workingFilters.get(type)) {
     workingFilters.set(type, new Set())
-    if (!multiSelection) filtersCount.value = filtersCount.value + 1
   }
   if (multiSelection === false) workingFilters.get(type)?.clear()
   workingFilters.get(type)?.add(value)
-  if (multiSelection) filtersCount.value = filtersCount.value + 1
 }
 
 function removeFilter(type: string, value: string, multiSelection: boolean = true) {
   if (workingFilters.get(type)) {
     workingFilters.get(type)?.delete(value)
     if (workingFilters.get(type)?.size === 0) workingFilters.delete(type)
-    filtersCount.value = filtersCount.value - 1
   }
 }
 
 function resetFilter() {
   workingFilters.clear()
-  filtersCount.value = 0
 }
 
 export function useFilter() {
