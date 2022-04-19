c49202e4bb2d61a7871c746af74ce57eda0e7b56
Robocat <neatfx@gmail.com>
2022-04-19 17:18:42 +0800

    douban api

--- renderer/services/douban-suggest-api.js
+++ renderer/services/douban-suggest-api.js
@@ -1,35 +1,25 @@
 import { chromium } from 'playwright';
 import restify from 'restify';
-import corsMiddleware from 'restify-cors-middleware2'
-import fs from 'fs';
 
 const browser = await chromium.launch({ headless: true });
 const context = await browser.newContext();
 const page = await context.newPage();
-const server = restify.createServer({
-  // certificate: fs.readFileSync('./server.crt'),
-  // key: fs.readFileSync('./server.key'),
-});
-const cors = corsMiddleware({
-  preflightMaxAge: 5, //Optional
-  origins: ['https://127.0.0.1:3000'],
-})
-
-// 跨域设置
-server.pre(cors.preflight)
-server.use(cors.actual)
+const server = restify.createServer();
 
 // 路由
-async function suggest(req, res, next) {
+server.get('/suggest/:keyword', async (req, res, next) => {
   const keyword = req.params.keyword
   const respObj = await page.goto('https://book.douban.com/j/subject_suggest?q=' + keyword);
   const jsonData = await respObj.json()
+
   res.header('Content-Type', 'application/json')
+  res.header('Access-Control-Allow-Origin', 'https://127.0.0.1:3000') // 跨域设置
+  
   console.log(jsonData)
   res.send(jsonData)
+
   next()
-}
-server.get('/suggest/:keyword', suggest);
+});
 
 server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
