f3cd09fd67381470bfe72dc1a9e8b5eeca067833
Robocat <neatfx@gmail.com>
2022-04-20 17:38:00 +0800

    u

--- renderer/services/douban-suggest-api.js
+++ renderer/services/douban-suggest-api.js
@@ -13,10 +13,10 @@ server.get('/suggest/:keyword', async (req, res, next) => {
   const jsonData = await respObj.json()
 
   res.header('Content-Type', 'application/json')
-  res.header('Access-Control-Allow-Origin', 'https://127.0.0.1:3000') // 跨域设置
+  res.header('Access-Control-Allow-Origin', '*') // 跨域设置
   
   console.log(jsonData)
-  res.send(jsonData)
+  res.send(JSON.stringify(jsonData))
 
   next()
 });
